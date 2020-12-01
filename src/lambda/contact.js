5c49f0691c83b1bfc019883b41213a3cee993cf0
Moises <moy_085@hotmail.com>
2020-12-02 10:49:13 +1100

    Update configuration file

--- src/lambda/contact.js
+++ src/lambda/contact.js
@@ -1,36 +1,31 @@
-// const sgMail = require("@sendgrid/mail");
-// const contactEmail = require("./emailTemplates/contactEmail");
+const sgMail = require("@sendgrid/mail");
+const contactEmail = require("./emailTemplates/contactEmail");
 exports.handler = async (event) => {
-  // const sendGridKey = process.env.SEND_GRID_KEY;
+  const sendGridKey = process.env.SEND_GRID_KEY;
 
-  // const sendEmail = (body) => {
-  //   const { property, email } = body;
+  const sendEmail = (body) => {
+    const { property, email } = body;
 
-  //   sgMail.setApiKey(sendGridKey);
-  //   const msg = {
-  //     to: "moiscye@gmail.com",
-  //     from: email,
-  //     subject: property,
-  //     text: "message field",
-  //     html: contactEmail(body),
-  //   };
+    sgMail.setApiKey(sendGridKey);
+    const msg = {
+      to: "moiscye@gmail.com",
+      from: email,
+      subject: property,
+      text: "message field",
+      html: contactEmail(body),
+    };
 
-  //   sgMail.send(msg);
-  //   return body;
-  // };
+    sgMail.send(msg);
+    return body;
+  };
 
   if (event.httpMethod == "POST") {
     let body = event.body ? JSON.parse(event.body) : {};
     let response;
     if (body) {
-      // body = sendEmail(body);
+      body = sendEmail(body);
       response = {
         statusCode: 200,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers":
-            "Origin, X-Requested-With, Content-Type, Accept",
-        },
         body: JSON.stringify(body),
       };
     }
