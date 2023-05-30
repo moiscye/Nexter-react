1124b5ccd642518a8fb879361257e234e3ff3d72
Moises Cruz <moy_085@hotmail.com>
2023-05-30 04:25:56 +0000

    remove headers

--- src/lambda/contact.js
+++ src/lambda/contact.js
@@ -2,16 +2,12 @@ const sgMail = require("@sendgrid/mail");
 const contactEmail = require("./emailTemplates/contactEmail");
 exports.handler = async (event) => {
   const sendGridKey = process.env.SEND_GRID_KEY;
-  console.log("Event:", event.httpMethod);
-  const headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Content-Type",
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-  };
+  // console.log("Event:", event.httpMethod);
+
   if (event.httpMethod === "OPTIONS") {
     return {
       statusCode: 200,
-      headers,
+      
       body: JSON.stringify({ message: "Successful preflight call." }),
     };
   } else if (event.httpMethod == "POST") {
@@ -21,7 +17,7 @@ exports.handler = async (event) => {
       body = await sendEmail(body);
       response = {
         statusCode: 200,
-        headers,
+        
         body: JSON.stringify(body),
       };
     }
