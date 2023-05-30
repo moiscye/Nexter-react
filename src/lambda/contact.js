8755af62eb1daef422e7916bb8b36f6e0bed42e4
Moises Cruz <moy_085@hotmail.com>
2023-05-30 04:40:58 +0000

    update config file

--- src/lambda/contact.js
+++ src/lambda/contact.js
@@ -2,12 +2,16 @@ const sgMail = require("@sendgrid/mail");
 const contactEmail = require("./emailTemplates/contactEmail");
 exports.handler = async (event) => {
   const sendGridKey = process.env.SEND_GRID_KEY;
-  // console.log("Event:", event.httpMethod);
-
+  console.log("Event:", event.httpMethod);
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
+  };
   if (event.httpMethod === "OPTIONS") {
     return {
       statusCode: 200,
-      
+      headers,
       body: JSON.stringify({ message: "Successful preflight call." }),
     };
   } else if (event.httpMethod == "POST") {
@@ -17,7 +21,7 @@ exports.handler = async (event) => {
       body = await sendEmail(body);
       response = {
         statusCode: 200,
-        
+        headers,
         body: JSON.stringify(body),
       };
     }
