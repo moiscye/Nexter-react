82cd1b45840c0cf840e6953d1dbb8e4007372d9f
Moises Cruz <moy_085@hotmail.com>
2023-05-30 04:44:47 +0000

    update config files

--- src/lambda/contact.js
+++ src/lambda/contact.js
@@ -6,7 +6,7 @@ exports.handler = async (event) => {
   const headers = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "Content-Type",
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
+    "Access-Control-Allow-Methods": "*"
   };
   if (event.httpMethod === "OPTIONS") {
     return {
