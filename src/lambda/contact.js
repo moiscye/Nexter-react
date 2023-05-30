4e2dd39d516e4fadaadc89184f52a8f1eda44bcf
Moises Cruz <moy_085@hotmail.com>
2023-05-30 02:53:13 +0000

    fix error in log

--- src/lambda/contact.js
+++ src/lambda/contact.js
@@ -2,7 +2,7 @@ const sgMail = require("@sendgrid/mail");
 const contactEmail = require("./emailTemplates/contactEmail");
 exports.handler = async (event) => {
   const sendGridKey = process.env.SEND_GRID_KEY;
-  console.log(JSON.parse(event.body));
+  console.log(event.body);
   const headers = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "Content-Type",
